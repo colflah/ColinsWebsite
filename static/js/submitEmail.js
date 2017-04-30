eab9161f4e4bd71edfae3a3663b432d32d308703
Colin Flaherty <colflah@nuevaschool.org>
2017-04-30 16:04:31 -0700

    Debug

--- static/js/submitEmail.js
+++ static/js/submitEmail.js
@@ -15,7 +15,7 @@ $(function() {
 		  url: "/mailinglist",
 		  data: payload,
 		  success: function () {
-		  	$('div#email_div').html('<div>Hi</div>');
+		  	$('div#email_div').empty().html('Hi');
 		    //$('div#email_div').html('<div id="emailSubmitted"></div>');
 		    //$("div#emailSubmitted").html('<p class="black"> Email submitted. Thank you!</p>').hide().fadeIn(1500);
 	  	  }
